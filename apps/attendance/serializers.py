f0428b8057957194d8a9b64f91d738f06a678e2b
Rumaiz <rumaiz447@gmail.com>
2025-09-22 22:32:53 +0530

    Serializer Modifications in Attendance

--- apps/attendance/serializers.py
+++ apps/attendance/serializers.py
@@ -2,7 +2,18 @@ from rest_framework import serializers
 from .models import Attendance
 
 class AttendanceSerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source="student.name", read_only=True)
+    subject_name = serializers.CharField(source="subject.subject_name", read_only=True)
+    teacher_name = serializers.CharField(source="teacher.name", read_only=True)
+
     class Meta:
         model = Attendance
-        fields = ['id', 'student', 'subject', 'teacher']
-        read_only_fields = ['teacher'] 
+        fields = [
+            'id',
+            'student', 'student_name',
+            'subject', 'subject_name',
+            'teacher', 'teacher_name',
+            'date', 'status', 'is_active',
+            'created_at', 'updated_at',
+        ]
+        read_only_fields = ['teacher', 'created_at', 'updated_at']
