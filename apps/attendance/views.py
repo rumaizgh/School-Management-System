329e786bfbf783b66b6c32a717a1a6b0260eeeeb
Rumaiz <rumaiz447@gmail.com>
2025-09-23 17:28:18 +0530

    test

--- apps/attendance/views.py
+++ apps/attendance/views.py
@@ -9,6 +9,7 @@ class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
     authentication_classes = [JWTAuthentication]
+    authentication_classes = [JWTAuthentication]
 
     def get_permissions(self):
         if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
