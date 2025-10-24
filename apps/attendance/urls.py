07b6649300c8a6f87c32ed46395839b54afe2508
Muhammad Rumaiz <rumaiz447@gmail.com>
2025-10-24 23:39:17 +0530

    Attendance Session Initialised

--- apps/attendance/urls.py
+++ apps/attendance/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AttendanceSessionViewSet
+from .views import AttendanceSessionViewSet, AtdSession
 
 router = DefaultRouter()
 router.register(r'attendance', AttendanceSessionViewSet, basename='attendance')
+router.register(r'atd', AtdSession, basename='atd')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('atd/', AtdSession.as_view({'get': 'list'}), name='atd')
 ]
