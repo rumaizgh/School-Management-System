5b39a8144a4be33a91e2b13537d16f172ce3fd8d
rumaizgh <rumaiz447@gmail.com>
2026-03-09 15:03:24 +0530

    serializer fix

--- apps/attendance/serializers.py
+++ apps/attendance/serializers.py
@@ -5,11 +5,11 @@ from apps.account.models import UserData
 class AttendanceSessionSerializer(serializers.ModelSerializer):
     teacher_name = serializers.CharField(source='teacher.name', read_only=True)
     subject_name = serializers.CharField(source='subject.subject_name', read_only=True)
-    # class_name = serializers.CharField(source='classs.classs', read_only=True)
+    classs_name = serializers.CharField(source='classs.classs', read_only=True)
 
     class Meta:
         model = AttendanceSession
-        fields = ['teacher', 'teacher_name', 'date', 'time', 'subject', 'subject_name']
+        fields = ['id','teacher', 'teacher_name', 'date', 'time', 'subject', 'subject_name', 'classs' , 'classs_name']
 
 class AttendanceRecordSerializer(serializers.ModelSerializer):
     class Meta:
