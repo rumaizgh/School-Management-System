d71ce74d9ab1e31eee7cef93eb9e640cf5c291a1
rumaizgh <rumaiz447@gmail.com>
2026-04-09 11:25:19 +0530

    Login issue solved

--- apps/account/models.py
+++ apps/account/models.py
@@ -60,10 +60,6 @@ class UserData(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
-    def save(self, *args, **kwargs):
-        if self.password:
-            self.set_password(self.password)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} - {self.email} "
