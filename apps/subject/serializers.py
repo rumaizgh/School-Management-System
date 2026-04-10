47d76c6b332b9d4bd78533837f17e2fc039150c7
rumaizgh <rumaiz447@gmail.com>
2026-04-10 17:25:43 +0530

    undo commit

--- apps/subject/serializers.py
+++ apps/subject/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from .models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
-    teacher = serializers.CharField(source='teacher.name', read_only=True)
-
     class Meta:
         model = Subject
-        fields = ['id','subject_name','subject_code','classs', 'teacher']
+        fields = '__all__'
