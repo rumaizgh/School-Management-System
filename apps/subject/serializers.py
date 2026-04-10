72fcc2e2ca4babae3e1e8ec177511a1af3ed2baf
rumaizgh <rumaiz447@gmail.com>
2026-04-10 17:19:46 +0530

    added teacher name in viewsubject api

--- apps/subject/serializers.py
+++ apps/subject/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
+    teacher = serializers.CharField(source='teacher.name', read_only=True)
+
     class Meta:
         model = Subject
-        fields = '__all__'
+        fields = ['subject_name','subject_code','classs', 'teacher']
