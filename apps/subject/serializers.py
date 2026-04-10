bea5359a0140b9de036c1d2a8337db19393ab73d
rumaizgh <rumaiz447@gmail.com>
2026-04-10 17:30:32 +0530

    added teacher name in subject serializer

--- apps/subject/serializers.py
+++ apps/subject/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
+    teacher_name = serializers.CharField(source='teacher.name', read_only=True)
+
     class Meta:
         model = Subject
         fields = '__all__'
