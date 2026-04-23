1d5953a829f6bb33825ccce4c33e9983d2da053a
rumaizgh <rumaiz447@gmail.com>
2026-04-23 20:29:18 +0530

    UserDataResource Changes

--- apps/academics/resources.py
+++ apps/academics/resources.py
@@ -1,6 +1,7 @@
 from import_export import resources
 from .models import Fee
 from import_export import resources, fields
+from apps.account.models import UserData
 
 class FeeResource(resources.ModelResource):
 
@@ -16,3 +17,18 @@ class FeeResource(resources.ModelResource):
 
     def dehydrate_balance(self, obj):
         return obj.balance()
+    
+class UserDataResource(resources.ModelResource):
+    class Meta:
+        model = UserData
+        skip_unchanged = True
+        import_id_fields = ['email']
+
+    def get_import_data_kwargs(self, request, *args, **kwargs):
+        kwargs = super().get_import_data_kwargs(request, *args, **kwargs)
+        return kwargs
+
+    def before_import_row(self, row, **kwargs):
+        # Skip rows where email is empty
+        if not row.get('email'):
+            raise Exception("Skipping empty row")
