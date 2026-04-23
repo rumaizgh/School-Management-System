5c40c9ad5fb3b288766c67190bd6a225e7213b26
rumaizgh <rumaiz447@gmail.com>
2026-04-23 20:52:20 +0530

    removed unnecessary code from resource of academics

--- apps/academics/resources.py
+++ apps/academics/resources.py
@@ -18,13 +18,3 @@ class FeeResource(resources.ModelResource):
     def dehydrate_balance(self, obj):
         return obj.balance()
     
-class UserDataResource(resources.ModelResource):
-    class Meta:
-        model = UserData
-        skip_unchanged = True
-        import_id_fields = ['email']
-
-    def skip_row(self, instance, original, row, import_validation_errors=None):
-        if not row.get('email'):
-            return True  # silently skip blank rows
-        return super().skip_row(instance, original, row, import_validation_errors)
