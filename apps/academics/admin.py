82cb4a9a0f69f4d1fc0a954fa24fa684adcdae38
Muhammed Safvan-EK <safvan024safu@gmail.com>
2026-04-23 17:11:01 +0530

    add improt export feature of fee module

--- apps/academics/admin.py
+++ apps/academics/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 # Register your models here.
-admin.site.register([Batch, TimeTable, Fee, Payment])
+# admin.site.register([Batch, TimeTable, Fee, Payment])
+
+@admin.register(Batch, TimeTable, Fee, Payment)
+class UserDataAdmin(ImportExportModelAdmin):
+    pass
